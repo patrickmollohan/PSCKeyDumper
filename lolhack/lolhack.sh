5162b206743cdbce3da7f01091803f3f488bc7f7
Patrick Mollohan <14967142+patrickmollohan@users.noreply.github.com>
2019-06-23 02:34:34 -0400

    Initial Commit

--- lolhack/lolhack.sh
+++ lolhack/lolhack.sh
@@ -1,46 +1,36 @@
 #!/bin/sh
-LOG=/media/uninstall.log
+# Author: Patrick Mollohan (keybase.io/patrickmollohan)
+# Date: 06/24/2019
+# Purpose:
+# This script will dump the PlayStation Classic encryption keys into the loot
+# folder of your USB storage media. The PlayStation Classic will shut down after
+# the operation is complete.
+# Credits:
+# justMaku (gpghax)
 
-# Remount with write access
-mount -o rw,remount /gaadata
+led_flash_success() {
+  for ((i = 0; i < 3; i++)); do
+    echo 1 > /sys/class/leds/red/brightness
+    echo 0 > /sys/class/leds/green/brightness
+    sleep $1
+    echo 1 > /sys/class/leds/red/brightness
+    echo 1 > /sys/class/leds/green/brightness
+    sleep $1
+    echo 0 > /sys/class/leds/red/brightness
+    echo 1 > /sys/class/leds/green/brightness
+    sleep $1
+  done
+  echo 0 > /sys/class/leds/red/brightness
+  echo 0 > /sys/class/leds/green/brightness
+}
 
-# Clear log
-echo "" > $LOG
+killall -KILL sonyapp
 
-find /gaadata > /media/filelist.log
+cp -r /home/root /media/loot
+sync
 
-# Remove user settings file, system will recreate it on the next boot.
-rm /data/AppData/sony/ui/user.pre
-
-# Restore default DB
-if [ -e /media/databases/default.db  ]; then
-    cp /media/databases/default.db /gaadata/databases/regional.db
-    
-    echo "Restored default DB" >> $LOG
-else
-    echo "Missing default database file." > $LOG
-fi
-
-# Remove fucked up symlinks.
-rm /gaadata/1/1
-rm /gaadata/2/2
-rm /gaadata/3/3
-rm /gaadata/4/4
-rm /gaadata/5/5
-rm /gaadata/6/6
-rm /gaadata/7/7
-rm /gaadata/8/8
-rm /gaadata/9/9
-rm /gaadata/10/10
-rm /gaadata/11/11
-rm /gaadata/12/12
-rm /gaadata/13/13
-rm /gaadata/14/14
-rm /gaadata/15/15
-rm /gaadata/16/16
-rm /gaadata/17/17
-rm /gaadata/18/18
-rm /gaadata/19/19
-rm /gaadata/20/20
-
+umount /media
+led_flash_success 1
 shutdown -h now
+exit 0
+
